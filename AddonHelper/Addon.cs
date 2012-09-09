96b6fed4701590e49fa80ff27d9bf6fa4d666c51
Angelo Geels <spansjh@gmail.com>
2012-09-09 22:29:47 +0200

    Documented AddonHelper.Addon

--- AddonHelper/Addon.cs
+++ AddonHelper/Addon.cs
@@ -16,9 +16,19 @@ using System.Reflection;
 namespace AddonHelper {
     public enum DragCallbackType { Image, Animation, None }
     public class DragCallback {
+        /// <summary>
+        /// What kind of callback this is
+        /// </summary>
         public DragCallbackType Type;
 
+        /// <summary>
+        /// The image that the user dragged
+        /// </summary>
         public Image Image;
+
+        /// <summary>
+        /// A MemoryStream containing the buffer of the animation
+        /// </summary>
         public MemoryStream Animation;
     }
 
@@ -26,6 +36,10 @@ namespace AddonHelper {
         public static Random rnd = new Random();
         private Settings appSettings = new Settings("settings.txt");
 
+        /// <summary>
+        /// Writes to debug.txt
+        /// </summary>
+        /// <param name="str">Line of text</param>
         public void Debug(string str) {
             StreamWriter writer;
             if (File.Exists("debug.txt"))
@@ -38,6 +52,10 @@ namespace AddonHelper {
             writer.Dispose();
         }
 
+        /// <summary>
+        /// Returns the linux epoch time (seconds passed since 1/1/1970)
+        /// </summary>
+        /// <returns>Epoch time as a long</returns>
         public long Epoch() {
             return (DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000000;
         }
@@ -70,6 +88,10 @@ namespace AddonHelper {
             gfx.Dispose();
         }
 
+        /// <summary>
+        /// Enables or disables shortcuts in ClipUpload.
+        /// </summary>
+        /// <param name="enabled">Bool to enable or disable</param>
         public static void SetShortcuts(bool enabled) {
             Form form = null;
             foreach(Form f in Application.OpenForms) {
@@ -84,6 +106,19 @@ namespace AddonHelper {
             piEnabled.SetValue(fiListener.GetValue(form), enabled, null);
         }
 
+        /// <summary>
+        /// Formats a given filename, based on several template variables:
+        /// <list type="bullet">
+        ///     <item><term>%ADDON%</term><description>The name of the addon</description></item>
+        ///     <item><term>%DATE%</term><description>Current date in the usual .Net format except / is replaced with -.</description></item>
+        ///     <item><term>%TIME%</term><description>Current time in the usual .Net format except : is replaced with ..</description></item>
+        ///     <item><term>%EPOCH%</term><description>Current epoch time (amount of seconds passed since 1/1/1970)</description></item>
+        ///     <item><term>%EPOCHX%</term><description>Current epoch time in a hexadecimal format</description></item>
+        ///     <item><term>%FILENAME%</term><description>The original filename</description></item>
+        /// </list>
+        /// </summary>
+        /// <param name="origFilename">The original filename</param>
+        /// <returns>Formatted filename</returns>
         private string formatFilename(string origFilename) {
             string fnm = appSettings.GetString("BackupsFormat");
             fnm = fnm.Replace("%ADDON%", this.GetType().Name);
@@ -100,6 +135,10 @@ namespace AddonHelper {
             return fnm;
         }
 
+        /// <summary>
+        /// Make a backup of the given source file
+        /// </summary>
+        /// <param name="sourceFile">The source filename</param>
         public void Backup(string sourceFile) {
             if (appSettings.GetBool("BackupsEnabled")) {
                 string path = appSettings.GetString("BackupsPath");
@@ -113,6 +152,11 @@ namespace AddonHelper {
             }
         }
 
+        /// <summary>
+        /// Make a backup of the given file buffer as the current filename
+        /// </summary>
+        /// <param name="buffer">The file buffer</param>
+        /// <param name="filename">The filename</param>
         public void Backup(byte[] buffer, string filename) {
             if (appSettings.GetBool("BackupsEnabled")) {
                 string path = appSettings.GetString("BackupsPath");
@@ -125,6 +169,11 @@ namespace AddonHelper {
             }
         }
 
+        /// <summary>
+        /// Add a recently uploaded file to the upload log
+        /// </summary>
+        /// <param name="URL">The URL</param>
+        /// <param name="info">Additional info that will appear in the recent uploads list</param>
         public void AddLog(string URL, string info) {
             Form form = null;
             foreach (Form f in Application.OpenForms) {
@@ -148,18 +197,31 @@ namespace AddonHelper {
             writer.Dispose();
         }
 
+        /// <summary>
+        /// Drag to upload
+        /// </summary>
+        /// <param name="doneDragging">Dragging callback</param>
         public void Drag(Action<DragCallback> doneDragging) {
             FormDrag formDrag = new FormDrag(this);
             formDrag.DoneDragging = doneDragging;
             formDrag.ShowDialog();
         }
 
+        /// <summary>
+        /// Populate a keys combobox with possible keys to press
+        /// </summary>
+        /// <param name="comboBox">The combobox</param>
         public void PopulateKeysCombobox(ComboBox comboBox) {
             string[] keys = Enum.GetNames(typeof(Keys));
             foreach (string key in keys)
                 comboBox.Items.Add(key);
         }
 
+        /// <summary>
+        /// Generate an MD5 hash of the given string
+        /// </summary>
+        /// <param name="S">The string</param>
+        /// <returns>The MD5 hash of the given string</returns>
         public string MD5(string S) {
             MD5CryptoServiceProvider x = new MD5CryptoServiceProvider();
             byte[] bOut = x.ComputeHash(Encoding.UTF8.GetBytes(S));
@@ -169,6 +231,12 @@ namespace AddonHelper {
             return sb.ToString();
         }
 
+        /// <summary>
+        /// Returns a random string based on the length and the allowed characters given
+        /// </summary>
+        /// <param name="len">The length</param>
+        /// <param name="chars">Allowed characters</param>
+        /// <returns>Random string based on requirements</returns>
         public static string RandomString(int len, string chars = "abcdefghijklmnopqrstuvwxyz0123456789") {
             string ret = "";
             for (int i = 0; i < len; i++) {
@@ -181,22 +249,48 @@ namespace AddonHelper {
             return ret;
         }
 
+        /// <summary>
+        /// Returns a random filename based on the length and the allowed characters given
+        /// </summary>
+        /// <param name="len">The length</param>
+        /// <param name="chars">Allowed characters</param>
+        /// <returns>Random string based on requirements</returns>
         public string RandomFilename(int len, string chars = "abcdefghijklmnopqrstuvwxyz0123456789") {
             return RandomString(len, chars);
         }
 
+        /// <summary>
+        /// Returns a date string without any delimiters in the following order:
+        /// Day, month, year, hour, minute, second
+        /// </summary>
+        /// <returns>Non-delimitered date string</returns>
         public string getDateString() {
             return DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString() + DateTime.Now.Hour.ToString() + DateTime.Now.Minute.ToString() + DateTime.Now.Second.ToString();
         }
 
+        /// <summary>
+        /// Encode a string in base64
+        /// </summary>
+        /// <param name="input">The string to be encoded</param>
+        /// <returns>The encoded base64 string</returns>
         public string base64Encode(string input) {
             return Convert.ToBase64String(Encoding.ASCII.GetBytes(input));
         }
 
+        /// <summary>
+        /// Decode a base64 string
+        /// </summary>
+        /// <param name="input">The base64 string to be decoded</param>
+        /// <returns>The decoded string</returns>
         public string base64Decode(string input) {
             return Encoding.ASCII.GetString(Convert.FromBase64String(input));
         }
 
+        /// <summary>
+        /// Alternative of <see cref="M:System.Uri.EscapeDataString(System.String)"/> to allow for much longer data to be escaped
+        /// </summary>
+        /// <param name="Str">The string to be escaped</param>
+        /// <returns>The escaped string</returns>
         public string LongDataEscape(string Str) {
             string Output = "";
             int ByteCount = 32766;
@@ -212,10 +306,21 @@ namespace AddonHelper {
             return Output;
         }
 
+        /// <summary>
+        /// Get a string between 2 other strings from the source string
+        /// </summary>
+        /// <param name="Source">The source string</param>
+        /// <param name="Str1">The first string, for example: "&lt;link&gt;"</param>
+        /// <param name="Str2">The second string, for example: "&lt;/link&gt;"</param>
+        /// <returns>Returns the string between Str1 and Str2</returns>
         public string GetBetween(string Source, string Str1, string Str2) {
             return Source.Split(new string[] { Str1, Str2 }, StringSplitOptions.None)[Source.Contains(Str1) ? 1 : 0];
         }
 
+        /// <summary>
+        /// Get a WebProxy object from the global proxy settings in the ClipUpload application
+        /// </summary>
+        /// <returns>The WebProxy object</returns>
         public WebProxy GetProxy() {
             if (appSettings.GetBool("ProxyEnabled")) {
                 string hostName = appSettings.GetString("ProxyHost");
@@ -240,6 +345,10 @@ namespace AddonHelper {
         [DllImport("user32.dll")]
         public static extern IntPtr GetOpenClipboardWindow();
 
+        /// <summary>
+        /// Safely sets the clipboard text. Refrain from using Clipboard.SetText directly, instead, use this function!
+        /// </summary>
+        /// <param name="Text">The text to set the clipboard to</param>
         public void SetClipboardText(string Text) {
             int timeout = 5;
             while (timeout-- > 0) {
@@ -254,6 +363,11 @@ namespace AddonHelper {
             }
         }
 
+        /// <summary>
+        /// Returns the image encoder for the given ImageFormat
+        /// </summary>
+        /// <param name="format">The ImageFormat to get the encoder for</param>
+        /// <returns>The ImageCodecInfo object</returns>
         public ImageCodecInfo GetEncoder(ImageFormat format) {
             ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
             foreach (ImageCodecInfo codec in codecs) {
@@ -277,10 +391,21 @@ namespace AddonHelper {
 
             private Settings appSettings = new Settings("settings.txt");
 
+            /// <summary>
+            /// Shows a progressbar window.
+            /// </summary>
+            /// <param name="Filename">The filename</param>
+            /// <param name="Filesize">The filesize</param>
             public void Start(string Filename, long Filesize) {
                 this.Start(Filename, Filesize, true);
             }
 
+            /// <summary>
+            /// Shows a progressbar window.
+            /// </summary>
+            /// <param name="Filename">The filename</param>
+            /// <param name="Filesize">The filesize</param>
+            /// <param name="DisplaySpeed">Whether or not to display the speed</param>
             public void Start(string Filename, long Filesize, bool DisplaySpeed) {
                 Addon.SetShortcuts(false);
 
@@ -360,6 +485,10 @@ namespace AddonHelper {
                 this.Form.progressBar.Value = percVal;
             }
 
+            /// <summary>
+            /// Set the current progress
+            /// </summary>
+            /// <param name="currentLocation">The current location in the file</param>
             public void Set(int currentLocation) {
                 if (!Canceled && this.Form != null) {
                     try {
@@ -370,6 +499,9 @@ namespace AddonHelper {
                 }
             }
 
+            /// <summary>
+            /// Close the progressbar window
+            /// </summary>
             public void Done() {
                 Addon.SetShortcuts(true);
 
@@ -384,6 +516,9 @@ namespace AddonHelper {
             }
         }
 
+        /// <summary>
+        /// Used to invoke a progressbar window
+        /// </summary>
         public cProgressBar ProgressBar = new cProgressBar();
     }
 }
